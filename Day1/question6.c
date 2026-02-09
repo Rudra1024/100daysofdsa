5be0285f453b658b1165b8df7314dcc4708c9176
Rudra1024 <226049507+Rudra1024@users.noreply.github.com>
2026-02-09 22:11:40 +0530

    main

---
+++ Day1/question6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Print first element
+    if(n > 0) {
+        printf("%d", arr[0]);
+    }
+    
+    // Print remaining unique elements
+    for(int i = 1; i < n; i++) {
+        if(arr[i] != arr[i-1]) {
+            printf(" %d", arr[i]);
+        }
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
